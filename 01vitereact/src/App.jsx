25254ed9aa3eaf9958d756ce72839dadebe057a2
prathamesh patil <patilprathamesh696@gmail.com>
2023-10-07 21:11:29 +0530

    first react project

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+import Chai from "./Chai.jsx"
+function App() {
+  return (
+    <>
+      <Chai />
+      <h3>Byee</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ratione earum qui.</p>
+    </>
+  )
+}
+export default App
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,6 @@
+function Chai () {
+    return(
+        <h3>Chai is Ready! Prthmsh Patilllll</h3>
+    );
+}
+export default Chai
